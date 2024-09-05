c5acf600ed2605bc75cb99df8b46df37b7fc4b67
LOKESH999 <lokiapm888@gmail.com>
2024-09-05 12:36:46 +0530

    s

---
+++ src-tauri/src/db.rs
@@ -0,0 +1,129 @@
+use crate::{models::*, schema::batch};
+use crate::schema::{auth, batch_list, customers, e_ids, order_list, orders};
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use diesel::result::Error;
+use dotenv::dotenv;
+use std::env; 
+use chrono::{NaiveDate,NaiveDateTime,Utc};
+
+pub fn establish_conn()->PgConnection{
+    dotenv().ok();
+    let url=env::var("DATABASE_URL").unwrap();
+    PgConnection::establish(url.as_str()).unwrap()
+}
+
+pub fn auth(conn:&mut PgConnection,username:String,passwd:String)->Result<Auth,Error>{
+    auth::dsl::auth
+    .filter(auth::dsl::username.eq(username))
+    .filter(auth::dsl::passwd.eq(passwd))
+    .limit(1)
+    .get_result::<Auth>(conn)
+}
+
+pub fn add_batch(conn:&mut PgConnection,order_id:i64,total:f64,d_date:NaiveDate)->Result<Batch,Error>{
+    diesel::insert_into(batch::table)
+    .values(Batch{
+        id:None,
+        order_id:order_id,
+        total:total,
+        d_date:d_date
+    }).returning(Batch::as_returning())
+    .get_result(conn)
+}
+
+pub fn add_batch_list(conn:&mut PgConnection,data:Vec<BList>,order_id:i64,batch_id:i64)->Result<Vec<BatchList>,Error>{
+    diesel::insert_into(batch_list::table)
+    .values(data.into_iter().map(|f| f.into_batch_list(order_id, batch_id)).collect::<Vec<BatchList>>())
+    .returning(BatchList::as_returning())
+    .get_results(conn)
+} 
+
+pub fn add_orders(conn:&mut PgConnection,val:Order)->Result<Order,Error>{
+    diesel::insert_into(orders::table)
+    .values(val)
+    .returning(Order::as_returning())
+    .get_result(conn)
+    // todo!()
+}
+ 
+pub fn add_order_list(conn:&mut PgConnection,order_id:i64,val:Vec<OList>)->Result<Vec<OrderList>,Error>{
+     diesel::insert_into(order_list::table)
+     .values(val.into_iter().map(|x| x.into_order_list(order_id)).collect::<Vec<OrderList>>())
+     .returning(OrderList::as_returning())
+     .get_results(conn)
+}
+
+pub fn update_orders(conn:&mut PgConnection,order_id:i64,m_batches:bool,total:f64,pending:f64)->Result<Order,Error>{
+    diesel::update(orders::table)
+    .filter(orders::order_id.eq(order_id))
+    .set((
+        orders::m_batches.eq(m_batches),
+        orders::amount.eq(total),
+        orders::pending_amount.eq(pending)
+    )).returning(Order::as_returning())
+    .get_result(conn)
+    // todo!()
+}
+
+pub fn update_batch(conn:&mut PgConnection,id:i64,total:f64,d_date:NaiveDate)->Result<Batch,Error>{
+    diesel::update(batch::table)
+    .filter(batch::id.eq(id))
+    .set((
+        batch::total.eq(total),
+        batch::d_date.eq(d_date)
+    )).returning(Batch::as_returning())
+    .get_result(conn)
+}
+
+// pub fn update_batch_list(conn:&mut PgConnection,)
+
+pub fn update_customer(id:i32,name:String,hst:String,address:String,phone_no:i64,email:String,conn:&mut PgConnection)->Result<Customer, Error>{
+    diesel::update(customers::table)
+    .filter(customers::cust_id.eq(id))
+    .set((
+        customers::name.eq(name),
+        customers::hst.eq(hst),
+        customers::address.eq(address),
+        customers::phone.eq(phone_no),
+        customers::primary_email.eq(email)
+    )).returning(Customer::as_returning()).get_result(conn)
+}
+pub fn get_all_cust(conn:&mut PgConnection)->Result<Vec<Customer>,Error>{
+    customers::dsl::customers.load::<Customer>(conn)
+}
+
+pub fn get_all_orders(conn:&mut PgConnection)->Result<Vec<Order>,Error>{
+    orders::dsl::orders.load::<Order>(conn)
+}
+
+pub fn get_all_order_list(conn:&mut PgConnection)->Result<Vec<OrderList>,Error>{
+    order_list::dsl::order_list.load::<OrderList>(conn)
+}
+
+pub fn get_order_by_id(conn:&mut PgConnection,id:Option<i64>,cust_id:Option<i32>)->Result<Vec<Order>,String>{
+    if id.is_none() && cust_id.is_none(){
+        return Err("both order_id and customer_id is none".to_string());
+    }
+    match if id.is_none(){
+        orders::table.filter(orders::cust_id.eq(cust_id.unwrap())).load::<Order>(conn)
+    }else  {
+        orders::table.filter(orders::order_id.eq(id.unwrap())).load::<Order>(conn)
+    }{
+        Ok(x)=>Ok(x),
+        Err(x)=>Err(x.to_string())
+    }
+}
+
+pub fn get_order_list_by_id(conn:&mut PgConnection,id:i64)->Result<Vec<OrderList>,Error>{
+    order_list::table.filter(order_list::order_id.eq(id)).load::<OrderList>(conn)
+    // todo!()
+}
+
+pub fn orders_by_date(conn:&mut PgConnection,date:NaiveDate)->Result<Vec<Order>,Error>{
+    orders::table.filter(diesel::dsl::date(orders::order_date).eq(date)).load::<Order>(conn)
+}
+
+pub fn get_email_by_id(conn:&mut PgConnection,cust_id:i32)->Result<Vec<String>,Error>{
+    e_ids::table.filter(e_ids::cust_id.eq(cust_id)).select(e_ids::email).get_results::<String>(conn)
+}
---
+++ src-tauri/src/models.rs
@@ -0,0 +1,188 @@
+// @generated automatically by Diesel CLI.
+use crate::schema::*;
+use diesel::prelude::*;
+use serde::{Deserialize,Serialize};
+use chrono;
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = auth)]
+pub struct Auth {
+    pub username: String,
+    pub passwd: String,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = batch)]
+pub struct Batch {
+    pub id: Option<i64>,
+    pub order_id: i64,
+    pub total: f64,
+    pub d_date: chrono::NaiveDate,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = batch_list)]
+pub struct BatchList {
+    pub id: Option<i64>,
+    pub batch_id: i64,
+    pub order_id: i64,
+    pub oil: String,
+    pub brand: String,
+    pub n_weight: i32,
+    pub cases: i32,
+    pub bottles: i32,
+    pub cost: f64,
+    pub msg: String,
+    pub term: chrono::NaiveDate,
+}
+#[derive(Debug,Serialize,Deserialize)]
+pub struct BList {
+    pub oil: String,
+    pub brand: String,
+    pub n_weight: i32,
+    pub cases: i32,
+    pub bottles: i32,
+    pub cost: f64,
+    pub msg: String,
+    pub term: chrono::NaiveDate,
+}
+impl BList{
+    pub fn into_batch_list(self,order_id:i64,batch_id:i64)->BatchList{
+        BatchList{
+            id:None,
+            batch_id:batch_id,
+            order_id:order_id,
+            oil:self.oil,
+            brand:self.brand,
+            n_weight:self.n_weight,
+            cases:self.cases,
+            bottles:self.bottles,
+            cost:self.cost,
+            msg:self.msg,
+            term:self.term
+        }
+    }
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = brands)]
+pub struct Brand {
+    pub brand: String,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = customers)]
+pub struct Customer {
+    pub cust_id: Option<i32>,
+    pub name: String,
+    pub hst: String,
+    pub address: String,
+    pub primary_email: String,
+    pub phone:i64,
+    pub date: chrono::NaiveDateTime,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = e_ids)]
+pub struct EId {
+    pub email: String,
+    pub cust_id: i32,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = order_list)]
+pub struct OrderList {
+    pub id: Option<i64>,
+    pub order_id: i64,
+    pub oil: String,
+    pub brand: String,
+    pub cases: i32,
+    pub bottles: i32,
+    pub cost: f64,
+    pub n_weights: i32,
+    pub msg: String,
+    pub term: chrono::NaiveDate,
+}
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct OList{
+    pub oil: String,
+    pub brand: String,
+    pub cases: i32,
+    pub bottles: i32,
+    pub cost: f64,
+    pub n_weights: i32,
+    pub msg: String,
+    pub term: chrono::NaiveDate,
+}
+
+impl OList{
+    pub fn into_order_list(self,order_id:i64)->OrderList{
+        OrderList{
+            id:None,
+            order_id:order_id,
+            oil:self.oil,
+            brand:self.brand,
+            n_weights:self.n_weights,
+            cases:self.cases,
+            bottles:self.bottles,
+            cost:self.cost,
+            msg:self.msg,
+            term:self.term
+        }
+    }
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = orders)]
+pub struct Order {
+    pub order_id: Option<i64>,
+    pub cust_id: i32,
+    pub m_batches: bool,
+    pub amount: f64,
+    pub pending_amount: f64,
+    pub order_date: chrono::NaiveDateTime,
+    pub due_date: chrono::NaiveDate,
+    pub status: i16,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = packages)]
+pub struct Package {
+    pub descs: String,
+    pub bottel_in_pallet: i16,
+    pub price: f32,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = payments)]
+pub struct Payment {
+    pub id: Option<i64>,
+    pub order_id: i64,
+    pub amount: f64,
+    pub method: String,
+    pub time: chrono::NaiveDateTime,
+    pub status: i16,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = shipping_details)]
+pub struct ShippingDetail {
+    pub ship_id: Option<i64>,
+    pub carrier: String,
+    pub tracking_no: String,
+    pub eda: chrono::NaiveDate,
+    pub cost: f32,
+    pub n_weight: i32,
+    pub address: String,
+    pub batch_id: i64,
+}
+
+#[derive(Queryable,Selectable,Insertable,Debug,Serialize,Deserialize)]
+#[diesel(table_name = user_watchdog)]
+pub struct UserWatchdog {
+    pub id: Option<i64>,
+    pub username: String,
+    pub date_time: chrono::NaiveDateTime,
+    pub descs: String,
+}
